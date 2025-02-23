109cf1ec49a7b85d26fbefcccacde44858143b1a
gabriel.costa <gabriel.costa@demander.com.br>
2025-02-23 08:46:52 -0300

    Cria o projeto: BBC Colaborator

---
+++ android/app/src/main/java/com/alme/ti/bbc_colaborador/MainActivity.java
@@ -0,0 +1,6 @@
+package com.alme.ti.bbc_colaborador;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:bbc_colaborador/ui/login/page_login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PageLogin());
+}
